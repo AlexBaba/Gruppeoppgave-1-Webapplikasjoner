c12325a508521f617618ad24f59ecc7d9e89964f
Jørgen Kann <jorgen@kann.no>
2015-10-04 13:24:40 +0200

    Updated routes. Now maps more closely to the whole controller structure.

--- Nettbutikk/Nettbutikk/App_Start/RouteConfig.cs
+++ Nettbutikk/Nettbutikk/App_Start/RouteConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nettbutikk.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,9 +17,68 @@ namespace Nettbutikk
             routes.MapMvcAttributeRoutes();
 
             routes.MapRoute(
+                name: "ProductDetails",
+                url: "product/{id:Guid}",
+                defaults: new {
+                    controller = "ProductsController",
+                    action = "Details",
+                    id = UrlParameter.Optional
+                }
+            );
+
+            routes.MapRoute(
+                name: "Products",
+                url: "products/{id}/{action}",
+                defaults: new {
+                    controller = "ProductsController",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                }
+            );
+
+            routes.MapRoute(
+                name: "Category",
+                url: "category/{name:string}",
+                defaults: new { controller = "CategoriesController", action = "show" }
+            );
+
+            routes.MapRoute(
+                name: "Categories",
+                url: "categories/{action}/{id}",
+                defaults: new
+                {
+                    controller = "CategoriesController",
+                    action = "Index"
+                }
+            );
+
+            routes.MapRoute(
+                "Search",
+                url: "search/{term:string}",
+                defaults: new {
+                    controller = "Home",
+                    action = "Search"
+                }
+            );
+
+            routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}",
+                defaults: new {
+                    controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "admin/products/{action}",
+                defaults: new {
+                    controller = "ProductsController",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                }
             );
         }
     }
