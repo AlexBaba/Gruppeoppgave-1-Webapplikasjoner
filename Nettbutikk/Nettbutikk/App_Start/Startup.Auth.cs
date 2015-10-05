58fe23ef312b13618db6a11f34f49271c3e180ca
Jørgen Kann <jorgen@kann.no>
2015-10-05 08:38:47 +0200

    Updated identity config and added views&scripts.
    Updated authentication to enable Facebook & Google-signin.
    
    Also added several scaffolded versions of views and som more newer
    versions of several scripts.
    Lastly removed old migrations as there were some errors in them. Will be
    regenerated later when model is somewhat more correct.

--- Nettbutikk/Nettbutikk/App_Start/Startup.Auth.cs
+++ Nettbutikk/Nettbutikk/App_Start/Startup.Auth.cs
@@ -6,7 +6,8 @@ using Microsoft.Owin.Security.Cookies;
 using Owin;
 using Nettbutikk.Models;
 using Nettbutikk.DAL;
-using Nettbutikk.Infrastructure;
+using Microsoft.Owin.Security.Google;
+using System.Web.Configuration;
 
 namespace Nettbutikk
 {
@@ -56,13 +57,29 @@ namespace Nettbutikk
                 DefaultAuthenticationTypes.TwoFactorCookie,
                 TimeSpan.FromMinutes(5));
 
-            // Enables the application to remember the second login
-            // verification factor such as phone or email. Once you check this
-            // option, your second step of verification during the login
-            // process will be remembered on the device where you logged in
-            // from. This is similar to the RememberMe option when you log in.
-            app.UseTwoFactorRememberBrowserCookie(
-                DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+            // Enables the application to remember the second login verification factor such as phone or email.
+            // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
+            // This is similar to the RememberMe option when you log in.
+            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+
+            // Uncomment the following lines to enable logging in with third party login providers
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //app.UseTwitterAuthentication(
+            //   consumerKey: "",
+            //   consumerSecret: "");
+
+            app.UseFacebookAuthentication(
+               appId: WebConfigurationManager.AppSettings["facebook-api-app-id"],
+               appSecret: WebConfigurationManager.AppSettings["facebook-api-app-secret"]);
+
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = WebConfigurationManager.AppSettings["google-api-client-id"],
+                ClientSecret = WebConfigurationManager.AppSettings["google-api-client-secret"]
+            });
         }
     }
 }
