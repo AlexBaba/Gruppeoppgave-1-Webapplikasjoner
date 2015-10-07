4307f6cabc8c61ec96b5fab17cf13bc2f55c1836
Jørgen Kann <jorgen@kann.no>
2015-10-07 13:30:08 +0200

    Moved all of Oblig1_Nettbutikk into Nettbutikk namespace

---
+++ Nettbutikk/Nettbutikk/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Nettbutikk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nettbutikk.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.LoggedIn = false;
+            if(Session["LoggedIn"] != null)
+            {
+                ViewBag.LoggedIn = (bool)(Session["LoggedIn"]);
+            }
+
+            List<Category> categories = DB.AllCategories();
+            ViewBag.Categories = categories;
+
+            return View();
+        }
+    }
+}
